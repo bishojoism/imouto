e7dfeef69bfcdbc80e58adccbc6218d23ae0c792
bishojoism <bishojoism@163.com>
2026-07-19 16:56:34 +0800

    简化

--- imouto.js
+++ imouto.js
@@ -126,52 +126,9 @@ function _自动操作(w, 动作) {
     _浮动(w)
 }
 
-能力.完成 = (手动) => {
+function _没判断(xml, 手动, 操作) {
     if (手动) {
-        const w = floaty.window(
-            <vertical>
-                <text id="拖动">✥</text>
-                <button id="下一步" text="完成" />
-                <button id="跳出" text="跳出" />
-            </vertical>
-        )
-
-        _浮动(w)
-    }
-
-    throw new Error('完成')
-}
-
-能力.安装注册登录 = (应用名) => {
-    const w = floaty.window(
-        <vertical>
-            <text id="拖动">✥</text>
-            <text>{`请您：安装、注册、登录「${应用名}」。`}</text>
-            <button id="下一步" text="下一步" />
-            <button id="跳出" text="跳出" />
-        </vertical>
-    )
-
-    _浮动(w)
-}
-
-能力.进入主页 = (手动, 应用名, 包名) => {
-    const 操作 = () => {
-        global.shizuku(['shell', 'am', 'force-stop', 包名])
-        global.shizuku(['shell', 'monkey', '-p', 包名, '-c', 'android.intent.category.LAUNCHER', '1'])
-        global.sleep(800)
-    }
-
-    if (手动) {
-        const w = floaty.window(
-            <vertical>
-                <text id="拖动">✥</text>
-                <text>{`请您：进入「${应用名}」主页。`}</text>
-                <button id="自动操作" text="自动操作" />
-                <button id="下一步" text="下一步" />
-                <button id="跳出" text="跳出" />
-            </vertical>
-        )
+        const w = floaty.window(xml)
 
         _自动操作(w, () => {
             操作()
@@ -181,76 +138,10 @@ function _自动操作(w, 动作) {
     }
 }
 
-能力.回到上一页 = (手动, 页名) => {
-    const 操作 = () => {
-        global.shizuku(['shell', 'input', 'keyevent', 'KEYCODE_BACK'])
-        global.sleep(800)
-    }
-
-    if (手动) {
-        const w = floaty.window(
-            <vertical>
-                <text id="拖动">✥</text>
-                <text>{`请您：返回「${页名}」。`}</text>
-                <button id="自动操作" text="自动操作" />
-                <button id="下一步" text="下一步" />
-                <button id="跳出" text="跳出" />
-            </vertical>
-        )
-
-        _自动操作(w, () => {
-            操作()
-        })
-    } else {
-        操作()
-    }
-}
-
-能力.向下滚动 = (手动) => {
-    const 操作 = () => {
-        global.shizuku(['shell', 'input', 'swipe', device.width * 0.5, device.height * 0.6, device.width * 0.5, device.height * 0.4, 1000])
-        global.sleep(800)
-    }
-
-    if (手动) {
-        const w = floaty.window(
-            <vertical>
-                <text id="拖动">✥</text>
-                <text>{`请您：向下滚动。`}</text>
-                <button id="自动操作" text="自动操作" />
-                <button id="下一步" text="下一步" />
-                <button id="跳出" text="跳出" />
-            </vertical>
-        )
-
-        _自动操作(w, () => {
-            操作()
-        })
-    } else {
-        操作()
-    }
-}
-
-能力.检查 = (手动, 内容) => {
-    const 操作 = () => {
-        return _视觉(`请问：是否「${内容}」？`, { type: 'boolean' })
-    }
-
+function _有判断(xml, 手动, 操作) {
     let 结果
     if (手动) {
-        const w = floaty.window(
-            <vertical>
-                <text id="拖动">✥</text>
-                <text>{`请问：是否「${内容}」？`}</text>
-                <horizontal>
-                    <button id="是" text="是" />
-                    <button id="否" text="否" />
-                </horizontal>
-                <button id="自动操作" text="自动操作" />
-                <button id="下一步" text="下一步" />
-                <button id="跳出" text="跳出" />
-            </vertical>
-        )
+        const w = floaty.window(xml)
 
         w.是.click(() => {
             结果 = true
@@ -288,8 +179,95 @@ function _自动操作(w, 动作) {
     return 结果
 }
 
+能力.完成 = (手动) => {
+    if (手动) {
+        _浮动(floaty.window(
+            <vertical>
+                <text id="拖动">✥</text>
+                <button id="下一步" text="完成" />
+                <button id="跳出" text="跳出" />
+            </vertical>
+        ))
+    }
+
+    throw new Error('完成')
+}
+
+能力.安装注册登录 = (应用名) => {
+    _浮动(floaty.window(
+        <vertical>
+            <text id="拖动">✥</text>
+            <text>{`请您：安装、注册、登录「${应用名}」。`}</text>
+            <button id="下一步" text="下一步" />
+            <button id="跳出" text="跳出" />
+        </vertical>
+    ))
+}
+
+能力.进入主页 = (手动, 应用名, 包名) => {
+    _没判断(<vertical>
+        <text id="拖动">✥</text>
+        <text>{`请您：进入「${应用名}」主页。`}</text>
+        <button id="自动操作" text="自动操作" />
+        <button id="下一步" text="下一步" />
+        <button id="跳出" text="跳出" />
+    </vertical>, 手动, () => {
+        global.shizuku(['shell', 'am', 'force-stop', 包名])
+        global.shizuku(['shell', 'monkey', '-p', 包名, '-c', 'android.intent.category.LAUNCHER', '1'])
+        global.sleep(800)
+    })
+}
+
+能力.回到上一页 = (手动, 页名) => {
+    _没判断(<vertical>
+        <text id="拖动">✥</text>
+        <text>{`请您：返回「${页名}」。`}</text>
+        <button id="自动操作" text="自动操作" />
+        <button id="下一步" text="下一步" />
+        <button id="跳出" text="跳出" />
+    </vertical>, 手动, () => {
+        global.shizuku(['shell', 'input', 'keyevent', 'KEYCODE_BACK'])
+        global.sleep(800)
+    })
+}
+
+能力.向下滚动 = (手动) => {
+    _没判断(<vertical>
+        <text id="拖动">✥</text>
+        <text>{`请您：向下滚动。`}</text>
+        <button id="自动操作" text="自动操作" />
+        <button id="下一步" text="下一步" />
+        <button id="跳出" text="跳出" />
+    </vertical>, 手动, () => {
+        global.shizuku(['shell', 'input', 'swipe', device.width * 0.5, device.height * 0.6, device.width * 0.5, device.height * 0.4, 1000])
+        global.sleep(800)
+    })
+}
+
+能力.检查 = (手动, 内容) => {
+    return _有判断(<vertical>
+        <text id="拖动">✥</text>
+        <text>{`请问：是否「${内容}」？`}</text>
+        <horizontal>
+            <button id="是" text="是" />
+            <button id="否" text="否" />
+        </horizontal>
+        <button id="自动操作" text="自动操作" />
+        <button id="下一步" text="下一步" />
+        <button id="跳出" text="跳出" />
+    </vertical>, 手动, () => {
+        return _视觉(`请问：是否「${内容}」？`, { type: 'boolean' })
+    })
+}
+
 能力.点击 = (手动, 元素) => {
-    const 操作 = () => {
+    _没判断(<vertical>
+        <text id="拖动">✥</text>
+        <text>{`请您：点击「${元素}」。`}</text>
+        <button id="自动操作" text="自动操作" />
+        <button id="下一步" text="下一步" />
+        <button id="跳出" text="跳出" />
+    </vertical>, 手动, () => {
         const [x, y] = _视觉(`请问：「${元素}」坐标？`, {
             type: 'array',
             description: '[x, y]',
@@ -303,29 +281,21 @@ function _自动操作(w, 动作) {
         })
         global.shizuku(['shell', 'input', 'tap', Math.floor(x * device.width / 1000), Math.floor(y * device.height / 1000)])
         global.sleep(800)
-    }
-
-    if (手动) {
-        const w = floaty.window(
-            <vertical>
-                <text id="拖动">✥</text>
-                <text>{`请您：点击「${元素}」。`}</text>
-                <button id="自动操作" text="自动操作" />
-                <button id="下一步" text="下一步" />
-                <button id="跳出" text="跳出" />
-            </vertical>
-        )
-
-        _自动操作(w, () => {
-            操作()
-        })
-    } else {
-        操作()
-    }
+    })
 }
 
 能力.检查若是则先点击 = (手动, 内容, 元素) => {
-    const 操作 = () => {
+    return _有判断(<vertical>
+        <text id="拖动">✥</text>
+        <text>{`请问：是否「${内容}」？若是，则请您先：点击「${元素}」`}</text>
+        <horizontal>
+            <button id="是" text="是" />
+            <button id="否" text="否" />
+        </horizontal>
+        <button id="自动操作" text="自动操作" />
+        <button id="下一步" text="下一步" />
+        <button id="跳出" text="跳出" />
+    </vertical>, 手动, () => {
         const 返回 = _视觉(`请问：是否「${内容}」？若是，请问：「${元素}」坐标？若否，请输出null。`, {
             anyOf: [
                 {
@@ -350,62 +320,21 @@ function _自动操作(w, 动作) {
         global.shizuku(['shell', 'input', 'tap', Math.floor(x * device.width / 1000), Math.floor(y * device.height / 1000)])
         global.sleep(800)
         return true
-    }
-
-    let 结果
-    if (手动) {
-        const w = floaty.window(
-            <vertical>
-                <text id="拖动">✥</text>
-                <text>{`请问：是否「${内容}」？若是，则请您先：点击「${元素}」`}</text>
-                <horizontal>
-                    <button id="是" text="是" />
-                    <button id="否" text="否" />
-                </horizontal>
-                <button id="自动操作" text="自动操作" />
-                <button id="下一步" text="下一步" />
-                <button id="跳出" text="跳出" />
-            </vertical>
-        )
-
-        w.是.click(() => {
-            结果 = true
-            w.是.enabled = false
-            w.否.enabled = true
-            w.下一步.enabled = true
-        })
-        w.否.click(() => {
-            结果 = false
-            w.否.enabled = false
-            w.是.enabled = true
-            w.下一步.enabled = true
-        })
-        ui.run(() => {
-            w.下一步.enabled = false
-        })
-
-        _自动操作(w, () => {
-            结果 = 操作()
-            ui.run(() => {
-                if (结果) {
-                    w.是.enabled = false
-                    w.否.enabled = true
-                } else {
-                    w.否.enabled = false
-                    w.是.enabled = true
-                }
-                w.下一步.enabled = true
-            })
-        })
-    } else {
-        结果 = 操作()
-    }
-
-    return 结果
+    })
 }
 
 能力.点击之后看见 = (手动, 元素, 文本) => {
-    const 操作 = () => {
+    return _有判断(<vertical>
+        <text id="拖动">✥</text>
+        <text>{`请您：点击「${元素}」。然后立刻判断：是否看见「${文本}」？`}</text>
+        <horizontal>
+            <button id="是" text="是" />
+            <button id="否" text="否" />
+        </horizontal>
+        <button id="自动操作" text="自动操作" />
+        <button id="下一步" text="下一步" />
+        <button id="跳出" text="跳出" />
+    </vertical>, 手动, () => {
         const [x, y] = _视觉(`请问：「${元素}」坐标？`, {
             type: 'array',
             description: '[x, y]',
@@ -430,62 +359,17 @@ function _自动操作(w, 动作) {
             }
         }
         return false
-    }
-
-    let 结果
-    if (手动) {
-        const w = floaty.window(
-            <vertical>
-                <text id="拖动">✥</text>
-                <text>{`请您：点击「${元素}」。然后立刻判断：是否看见「${文本}」？`}</text>
-                <horizontal>
-                    <button id="是" text="是" />
-                    <button id="否" text="否" />
-                </horizontal>
-                <button id="自动操作" text="自动操作" />
-                <button id="下一步" text="下一步" />
-                <button id="跳出" text="跳出" />
-            </vertical>
-        )
-
-        w.是.click(() => {
-            结果 = true
-            w.是.enabled = false
-            w.否.enabled = true
-            w.下一步.enabled = true
-        })
-        w.否.click(() => {
-            结果 = false
-            w.否.enabled = false
-            w.是.enabled = true
-            w.下一步.enabled = true
-        })
-        ui.run(() => {
-            w.下一步.enabled = false
-        })
-
-        _自动操作(w, () => {
-            结果 = 操作()
-            ui.run(() => {
-                if (结果) {
-                    w.是.enabled = false
-                    w.否.enabled = true
-                } else {
-                    w.否.enabled = false
-                    w.是.enabled = true
-                }
-                w.下一步.enabled = true
-            })
-        })
-    } else {
-        结果 = 操作()
-    }
-
-    return 结果
+    })
 }
 
 能力.点击之后输入 = (手动, 元素, 文本) => {
-    const 操作 = () => {
+    _没判断(<vertical>
+        <text id="拖动">✥</text>
+        <text>{`请您：点击「${元素}」。`}</text>
+        <button id="自动操作" text="自动操作" />
+        <button id="下一步" text="下一步" />
+        <button id="跳出" text="跳出" />
+    </vertical>, 手动, () => {
         const [x, y] = _视觉(`请问：「${元素}」坐标？`, {
             type: 'array',
             description: '坐标[x, y]',
@@ -503,25 +387,7 @@ function _自动操作(w, 动作) {
         global.setClip(文本)
         global.shizuku(['shell', 'input', 'keyevent', 'KEYCODE_PASTE'])
         global.sleep(800)
-    }
-
-    if (手动) {
-        const w = floaty.window(
-            <vertical>
-                <text id="拖动">✥</text>
-                <text>{`请您：点击「${元素}」。`}</text>
-                <button id="自动操作" text="自动操作" />
-                <button id="下一步" text="下一步" />
-                <button id="跳出" text="跳出" />
-            </vertical>
-        )
-
-        _自动操作(w, () => {
-            操作()
-        })
-    } else {
-        操作()
-    }
+    })
 }
 
 const 脚本 = {}
