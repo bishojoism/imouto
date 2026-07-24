8d0b1003fc2c82f3084c950ca301f6bb5fe4f5b9
bishojoism <bishojoism@163.com>
2026-07-25 07:08:39 +0800

    配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
     lang: 'zh-CN',
-    title: '网上处兄妹',
-    description: '一种安全、轻量、简洁、高效的异性相处范式',
+    title: '处兄妹攻略',
     themeConfig: {
         editLink: {
             pattern: 'https://github.com/bishojoism/imouto/edit/main/docs/:path'
